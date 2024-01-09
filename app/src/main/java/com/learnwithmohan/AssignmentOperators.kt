74fed247e9ed02f88bcb5c11af857769cc2b57ec
mkaliyap <mkaliypa@ford.com>
2024-01-09 12:09:40 +0530

    Assignment Operators

---
+++ app/src/main/java/com/learnwithmohan/AssignmentOperators.kt
@@ -0,0 +1,21 @@
+package com.learnwithmohan
+
+fun main() {
+
+    var total = 0
+
+    total += 5       //total = total + 5
+    println(total)   //Output 5
+
+    total -=3       //total = total - 3
+    println(total)  //Output 2
+
+    total *= 25     //total = total * 25
+    println(total)  //Output 50
+
+    total /= 5      //total = total / 5
+    println(total)  //Output 10
+
+    total %= 3      //total = total % 3
+    println(total)  //Output 1
+}
