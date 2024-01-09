59bc07493eedcb77bbfae877a709af4dae5e7cb3
mkaliyap <mkaliypa@ford.com>
2024-01-09 14:55:34 +0530

    Not In Operator

---
+++ app/src/main/java/com/learnwithmohan/NotInOperator.kt
@@ -0,0 +1,18 @@
+package com.learnwithmohan
+
+
+fun main() {
+
+    val numbers = listOf(1, 3, 10, 20)
+    val randomNumber = (1..20).random()
+
+    if (randomNumber in numbers) {
+        println("randomNumber is in the list of numbers!")
+    } else {
+        println("randomNumber is not in the list of numbers!")
+    }
+
+    if (randomNumber !in numbers) {
+        println("randomNumber is not in the list of numbers!")
+    }
+}
