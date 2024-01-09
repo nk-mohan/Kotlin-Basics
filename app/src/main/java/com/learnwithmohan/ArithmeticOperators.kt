b67c25c81960334bffb467ab6bc3842648673664
mkaliyap <mkaliypa@ford.com>
2024-01-09 12:01:17 +0530

    Arithmetic Operators

---
+++ app/src/main/java/com/learnwithmohan/ArithmeticOperators.kt
@@ -0,0 +1,20 @@
+package com.learnwithmohan
+
+fun main() {
+
+    val firstNumber = 19
+    val secondNumber = 3
+
+    val text1 = "Start"
+    val text2 = " Middle "
+    val text3 = "End"
+
+    println("+ - * / %")
+    println("$firstNumber + $secondNumber = ${firstNumber + secondNumber}")
+    println("$firstNumber - $secondNumber = ${firstNumber - secondNumber}")
+    println("$firstNumber * $secondNumber = ${firstNumber * secondNumber}")
+    println("$firstNumber / $secondNumber = ${firstNumber / secondNumber}")
+    println("$firstNumber % $secondNumber = ${firstNumber % secondNumber}")
+
+    println(text1 + text2 + text3)
+}
