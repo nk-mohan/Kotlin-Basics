7e944636b13ea1417515d7de292bcd60c2d1a280
nk-mohan <mohan71995@gmail.com>
2024-01-12 15:56:43 +0530

    Data Types

---
+++ app/src/main/java/com/learnwithmohan/DataTypes.kt
@@ -0,0 +1,30 @@
+package com.learnwithmohan
+
+
+var byteNumber: Byte = 17
+//Byte 8 bits | -128 to 127
+
+var shortNumber: Short = 250
+//Short 16 bits | -32768 to 32767
+
+var intNumber: Int = 60_000
+//Int 32 bits | -2,147,483,648 to 2,147,483,647
+
+var longNumber: Long = 7_324_986_754_985_984_674
+//Long 64 bits | -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+
+var floatNumber: Float = 12.98041f
+//Float 32 bits | 6-7 significant digits
+
+var doubleNumber: Double = 34.1283739298
+//Double 64 bits | 15-16 significant digits
+
+var letter: Char = 'a'
+//Char 16 bits | UTF-16 code unit (in most cases, this is one Unicode character,
+// but it might be just one half of a Unicode character)
+
+var sentence: String = "Hi, Welcome to learn with Mohan. 123 &"
+//String
+
+var isAvailable = "12"
+//Boolean 8 bits | true or false
